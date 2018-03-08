96ea29156107a0e3f405b04076b86e431ca2a298
unknown <laarajmoncif@gmail.com>
2018-03-08 20:54:05 +0000

    page error et rechercher

---
+++ 404error.php
@@ -0,0 +1,9 @@
+<?php
+ header ("HTTP / 1.0 404 Not Found");
+?>
+<html>
+  <head>
+    <title> Erreur 404 - Page non trouvée </title>
+  </head>
+  <body> 404 Error - Page non trouvée! </body>
+</html>
